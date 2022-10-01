9b4c9c6de04413cdcac62ead1f14bdc6f3e8e540
SwoxyComeBack <114838389+SwoxyComeBack@users.noreply.github.com>
2022-10-02 02:43:43 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -198,7 +198,7 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🔖 𝖲𝗍𝖺𝗋 𝖬𝗎𝗓𝗂𝗄 𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url=f"https://t.me/Starbotdestek"),
+                InlineKeyboardButton("🔖 Sahip𝗍", url=f"https://t.me/SwoxyComeBack"),
             ],
         ]
     )
@@ -260,7 +260,7 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🔖 𝖲𝗍𝖺𝗋 𝖬𝗎𝗓𝗂𝗄 𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url=f"https://t.me/Starbotdestek"),
+                InlineKeyboardButton("🔖 Sahip", url=f"https://t.me/SwoxyComeBack"),
             ],
         ]
     )
